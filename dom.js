7472b8ea18daec6b22bc63146efec1ea2ad60056
abhi620195 <abhi.arya97161>
2023-04-30 22:02:15 +0530

    QuerySelector

--- dom.js
+++ dom.js
@@ -2,14 +2,14 @@
 
 // console.log(document.domain)
 
-var items = document.getElementsByClassName('list-group-item')
-items[2].style.backgroundColor = "green";
+// var items = document.getElementsByClassName('list-group-item')
+// items[2].style.backgroundColor = "green";
 
-for(var i = 0;i<items.length;i++)
-{
-    items[i].style.fontWeight = "bold"
-    items[i].style.color = "red"
-}
+// for(var i = 0;i<items.length;i++)
+// {
+//     items[i].style.fontWeight = "bold"
+//     items[i].style.color = "red"
+// }
 
 // GETELEMENTBYCLASSNAME
 // var items =  document.getElementsByClassName('list-group-item')
@@ -27,15 +27,25 @@ for(var i = 0;i<items.length;i++)
 
 // GETELEMENTBYTAGNAME
 
-var li =  document.getElementsByTagName('li')
-console.log(li);
-console.log(li[1]);
-// li[1].textContent = 'Hello2';
+// var li =  document.getElementsByTagName('li')
+// console.log(li);
+// console.log(li[1]);
+// // li[1].textContent = 'Hello2';
 
-for(var i = 0;i<li.length;i++)
-{
-    li[i].style.backgroundColor = 'lightpink'
-}
+// for(var i = 0;i<li.length;i++)
+// {
+//     li[i].style.backgroundColor = 'lightpink'
+// }
 
-li[4].style.fontWeight = 'bold';
-li[4].style.backgroundColor = 'yellow';
+// li[4].style.fontWeight = 'bold';
+// li[4].style.backgroundColor = 'yellow';
+
+//QUERYSELECTOR //
+
+var second_item = document.querySelector('.list-group-item:nth-child(2)');
+
+second_item.style.backgroundColor = "green"
+
+var third_item = document.querySelector('.list-group-item:nth-child(3)');
+
+third_item.remove()
