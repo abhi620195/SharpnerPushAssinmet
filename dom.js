5616222e3fdb556a9152695672df468db765c530
abhi620195 <abhi.arya97161>
2023-04-30 20:54:31 +0530

    GetElementsbyClassname

--- dom.js
+++ dom.js
@@ -1,3 +1,12 @@
-console.dir(document)
+// console.dir(document)
 
-console.log(document.domain)
+// console.log(document.domain)
+
+var items = document.getElementsByClassName('list-group-item')
+items[2].style.backgroundColor = "green";
+
+for(var i = 0;i<items.length;i++)
+{
+    items[i].style.fontWeight = "bold"
+    items[i].style.color = "red"
+}
